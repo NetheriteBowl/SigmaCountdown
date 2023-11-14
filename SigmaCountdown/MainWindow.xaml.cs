3a0643e96e97992076e9c6bab5fe6908c87bc428
Netherite_Bowl <75211294+CN-Ironegg@users.noreply.github.com>
2023-11-14 13:39:52 +0800

    Add files via upload

---
+++ SigmaCountdown/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+
+namespace SigmaCountdown
+{
+
+
+    public partial class MainWindow : Window
+    {
+            public MainWindow()
+        {
+            InitializeComponent();
+            StartCountdown();
+        }
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+            this.Height = SystemParameters.WorkArea.Height / 4;
+            this.Width = 3.3*this.Height;
+            Left = SystemParameters.PrimaryScreenWidth - (this.Width);
+            Top = 3*SystemParameters.WorkArea.Height / 4;
+        }
+        private void StartCountdown()
+        {
+            DateTime deadline = Properties.Settings.Default.Date + new TimeSpan(8, 30, 0);
+            TimeSpan timeLeft = deadline - DateTime.Now;
+            int daysLeft = (int)timeLeft.TotalDays;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (sender, e) =>
+            {
+                timeLeft = deadline - DateTime.Now;
+                daysLeft = (int)timeLeft.TotalDays;
+
+                countdown.Text = daysLeft.ToString();
+                pCountdown.Text = daysLeft.ToString();
+                year.Text = Properties.Settings.Default.Year;
+            };
+
+            timer.Start();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void MenuItem_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            new SettingsWindow().Show();
+        }
+
+        private void MenuItem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
